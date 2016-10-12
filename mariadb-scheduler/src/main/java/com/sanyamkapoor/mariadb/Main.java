624e0fff1c9e234cfb4702fb8a5ecd32e76e6811
Sanyam Kapoor <1sanyamkapoor@gmail.com>
2016-10-12 10:02:04 +0530

    rearrange modules, use new apis

---
+++ mariadb-scheduler/src/main/java/com/sanyamkapoor/mariadb/Main.java
@@ -0,0 +1,24 @@
+package com.sanyamkapoor.mariadb;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.mesos.scheduler.SchedulerUtils;
+import org.apache.mesos.specification.DefaultService;
+
+import com.sanyamkapoor.mariadb.specification.MariaDbServiceSpecification;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    private static final String SERVICE_NAME = "mariadb";
+    private static final String ROLE = SchedulerUtils.nameToRole(SERVICE_NAME);
+    private static final String PRINCIPAL = SchedulerUtils.nameToPrincipal(SERVICE_NAME);
+
+    private static final int API_PORT = Integer.valueOf(System.getenv("PORT0"));
+
+    public static void main(String[] args) {
+        logger.info(String.format("starting %s scheduler with args %s", SERVICE_NAME, args));
+        new DefaultService(API_PORT).register(new MariaDbServiceSpecification(SERVICE_NAME));
+    }
+}
---
+++ mariadb-scheduler/src/main/java/com/sanyamkapoor/mariadb/config/CommandLineArgs.java
@@ -0,0 +1,15 @@
+package com.sanyamkapoor.mariadb.config;
+
+//import com.beust.jcommander.Parameter;
+
+public class CommandLineArgs {
+//    @Parameter(names = {"--help"}, description="Show this usage prompt", help = true)
+//    public boolean help;
+//
+//    // default value of 7 days
+//    @Parameter(names = {"--failover-timeout"}, description="Failover timeout for framework (in seconds)")
+//    public int failoverTimeout = 3600 * 7;
+//
+//    @Parameter(names = {"--zookeeper-url"}, description="Comma-separated list of zookeeper cluster nodes (<host:port>), e.g z1.local:2181,z2.local:2181", required = true)
+//    public String zookeeperUrl;
+}
